6d20ccd28f785ccc07819e99a99f438d40d67fa1
prabowohengky <prabowohengky6.6@gmail.com>
2026-07-25 14:33:40 +0700

    Update worker.js

--- worker.js
+++ worker.js
@@ -1,1 +1,9 @@
-
+export default {
+  async fetch(request, env) {
+    return new Response("Goflie Admin is working!", {
+      headers: {
+        "Content-Type": "text/plain"
+      }
+    });
+  }
+}
