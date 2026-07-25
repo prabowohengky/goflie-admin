1848753c94ee8520f7a0232935b885f5d08696e5
prabowohengky <prabowohengky6.6@gmail.com>
2026-07-25 18:20:24 +0700

    Update worker.js

--- worker.js
+++ worker.js
@@ -7,7 +7,7 @@ export default {
 if (url.pathname === "/" || url.pathname === "/index.html") {
 
 return Response.redirect(
-"https://https://goflie.xyz//goflie-admin/",
+"https://prabowohengky.github.io/goflie-admin/",
 302
 );
 
