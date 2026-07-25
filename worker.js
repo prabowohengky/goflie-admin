8ebf184fd6ee02a85623fbbdf1a87424a8d53c16
prabowohengky <prabowohengky6.6@gmail.com>
2026-07-25 14:36:57 +0700

    Update worker.js

--- worker.js
+++ worker.js
@@ -1,9 +1,23 @@
 export default {
   async fetch(request, env) {
-    return new Response("Goflie Admin is working!", {
-      headers: {
-        "Content-Type": "text/plain"
-      }
+    const url = new URL(request.url);
+
+    // Home
+    if (url.pathname === "/") {
+      return new Response("Goflie Admin API");
+    }
+
+    // List links
+    if (url.pathname === "/api/list") {
+      const result = await env.DB.prepare(
+        "SELECT * FROM links ORDER BY created_at DESC"
+      ).all();
+
+      return Response.json(result.results);
+    }
+
+    return new Response("404 Not Found", {
+      status: 404
     });
   }
 }
