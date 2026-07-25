9138baf1970b9b7c7aa4e06af0576ef26c2f9441
prabowohengky <prabowohengky6.6@gmail.com>
2026-07-25 14:43:18 +0700

    Update worker.js

--- worker.js
+++ worker.js
@@ -7,7 +7,26 @@ export default {
       return new Response("Goflie Admin API");
     }
 
-    // List links
+    // Create Short Link
+    if (url.pathname === "/api/create" && request.method === "POST") {
+      const body = await request.json();
+
+      await env.DB.prepare(
+        "INSERT INTO links (slug, original_url, created_at) VALUES (?, ?, ?)"
+      )
+        .bind(
+          body.slug,
+          body.original_url,
+          Date.now()
+        )
+        .run();
+
+      return Response.json({
+        success: true
+      });
+    }
+
+    // List Links
     if (url.pathname === "/api/list") {
       const result = await env.DB.prepare(
         "SELECT * FROM links ORDER BY created_at DESC"
